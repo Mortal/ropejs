8da8dac5ade2a212dad37d06f9a7db2a9c376ec6
Mathias Rav <rav@cs.au.dk>
2012-07-25 17:59:06 +0200

    Change main.js

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ var n = new Date().getTime() % 1000000000;
 while (true) {
   ++n;
   var s = n+'';
-  console.log("String:",t.stresstime(String, s, 2));
-  console.log("Rope:  ",t.stresstime(r.Leaf, s, 2), r.get_balance_counter());
+  console.log("String:",t.stresstime(String, s, 10));
+  console.log("Rope:  ",t.stresstime(r.Leaf, s, 10), r.get_balance_counter());
   r.reset_balance_counter();
 }
 // vim:set ts=2 sw=2 sts=2 et:
