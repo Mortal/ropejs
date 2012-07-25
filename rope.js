058734ab2933270c2f0cd28e9b68080e1800790b
Mathias Rav <rav@cs.au.dk>
2012-07-25 14:40:36 +0200

    Reorganize code, add notice about Boehm et al (1995)

--- rope.js
+++ rope.js
@@ -1,22 +1,10 @@
-function Concat(left, right) {
-  this.left = left;
-  this.right = right;
-  this.length = left.length + right.length;
-  this.depth = 1 + Math.max(left.depth, right.depth);
-}
-function Leaf(s) {
-  this.s = s;
-  this.length = s.length;
-  this.depth = 0;
-}
-function inorder_traversal(s, fn) {
-  if (s instanceof Concat) {
-    inorder_traversal(s.left, fn);
-    inorder_traversal(s.right, fn);
-  } else {
-    fn(s);
-  }
-}
+// An implementation of ropes in JavaScript.
+// Due to Boehm, Atkinson and Plass (1995)
+// "Ropes: an Alternative to Strings"
+
+///////////////////////////////////////////////////////////////////////////////
+// Fibonacci sequence helpers
+
 var fib = (function () {
   var ns = [0, 1];
   return function (n) {
@@ -35,18 +23,49 @@ function fibinv(len) {
   }
   return n; // == fibmax
 }
+
+///////////////////////////////////////////////////////////////////////////////
+// Internal rope nodes (concatenation nodes) and leaves
+
+function Concat(left, right) {
+  this.left = left;
+  this.right = right;
+  this.length = left.length + right.length;
+  this.depth = 1 + Math.max(left.depth, right.depth);
+}
+
+function Leaf(s) {
+  this.s = s;
+  this.length = s.length;
+  this.depth = 0;
+}
+
+function inorder_traversal(s, fn) {
+  if (s instanceof Concat) {
+    inorder_traversal(s.left, fn);
+    inorder_traversal(s.right, fn);
+  } else {
+    fn(s);
+  }
+}
+
+// "Short leaf" definition due to Boehm et al
 function short_leaf(s) {
   return !(s instanceof Concat) && s.length < 1;
 }
+
 Leaf.prototype.balance = function () {
   return this;
 };
+
 Leaf.prototype.balanced = function () {
-  return this.length >= fib(2);
+  return this.length >= fib(this.depth+2);
 };
+
 Concat.prototype.balanced = function () {
   return this.length >= fib(this.depth+2);
 };
+
 var balance_debug = true;
 Concat.prototype.balance = function (force) {
   if (!force && this.balanced()) return this;
@@ -96,12 +115,19 @@ Concat.prototype.balance = function (force) {
   }
   return result;
 };
+
+// ----------------------------------------------------------------------------
+// Convert to native JavaScript string
 Leaf.prototype.toString = function () {
   return this.s;
 };
 Concat.prototype.toString = function () {
   return this.left.toString() + this.right.toString();
 };
+
+// ----------------------------------------------------------------------------
+// The four rope operations.
+// 1. Fetch ith character.
 Leaf.prototype.charAt = function (pos) {
   return this.s.charAt(pos);
 };
@@ -109,6 +135,8 @@ Concat.prototype.charAt = function (pos) {
   if (pos < this.left.length) return this.left.charAt(pos);
   return this.right.charAt(pos - this.left.length);
 };
+
+// 2. Concatenate two ropes.
 Leaf.prototype.concat = function (str2) {
   if (this.length == 0) return str2;
   if (short_leaf(this) && short_leaf(str2)) {
@@ -122,6 +150,8 @@ Concat.prototype.concat = function (str2) {
   }
   return new Concat(this, str2);
 };
+
+// 3. Substring.
 Leaf.prototype.substring = function (pos1, pos2) {
   return new Leaf(this.s.substring(pos1, pos2));
 };
@@ -137,6 +167,10 @@ Concat.prototype.substring = function (pos1, pos2) {
       this.right.substring(0, pos2 - this.left.length));
 };
 
+// 4. Iterate over each character. See inorder_traversal() above.
+
+// ============================================================================
+// Conversion to and from JSON
 function BadJSON(msg, data) {this.msg = msg; this.data = data;}
 BadJSON.prototype.toString = function () { return this.msg; };
 function rope_from_json(data) {
@@ -144,5 +178,13 @@ function rope_from_json(data) {
   if (!data instanceof Array) throw new BadJSON("data neither Array nor String");
   if (data.length != 2) throw new BadJSON("data array has length "+data.length+", expected 2", data);
   return new Concat(rope_from_json(data[0]), rope_from_json(data[1]));
+}
+function rope_to_data(rope) {
+  if (rope instanceof Leaf) return rope.s;
+  if (rope instanceof Concat) return [rope_to_data(rope.left), rope_to_data(rope.right)];
+  return rope;
+}
+function rope_to_json(rope) {
+  return JSON.encode(rope_to_data(rope));
 }
 // vim:set ts=2 sw=2 sts=2 et:
